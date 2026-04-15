5368726c3280c4cad69fcaa0059b8a246fa5927e
jensenb <benedikt.jensen@pvatepla.com>
2026-04-15 12:53:59 +0200

    fix base url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SongEditor from './pages/editor/SongEditor';
 import PrintPreview from './pages/preview/PrintPreview';
+import {BASE_URL} from "./config.ts";
 
 function App() {
     return (
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter basename={BASE_URL}>
             <Routes>
                 <Route path="" element={<SongEditor />} />
                 <Route path="page-preview" element={<PrintPreview />} />
