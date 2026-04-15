6d2223ec43ad17c743663f54189f76c8c021d99e
jensenb <benedikt.jensen@pvatepla.com>
2026-04-15 11:59:58 +0200

    add loadEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    base: env.VITE_BASE_URL || '/',
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
-    }
-  },
-  build: {
-    outDir: 'dist'
-  },
-  plugins: [react()],
-})
+    },
+    build: {
+      outDir: 'dist'
+    },
+    plugins: [react()],
+  };
+});
