00f140d29a062c17b777f0a2c82c489dfd278c47
jensenb <benedikt.jensen@pvatepla.com>
2026-04-15 11:38:38 +0200

    base url helper

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/songbook-editor-react',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   build: {
     outDir: 'dist'
   },
